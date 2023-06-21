b67d62417c5dd4c732eb22a63de2a3037823b82d
Aiken Xie <xiea@m005.opa.vera.psc.edu>
2023-06-21 18:04:26 -0400

    some more changes for vera cluster

---
+++ data_dytt/generate_npz_old_v2.py
@@ -0,0 +1,184 @@
+"""
+script to generate npz files with event and PF candidates selections
+similar to DeepMET studies
+"""
+
+#from coffea.nanoaod import NanoEvents
+
+
+import awkward as ak
+from coffea.nanoevents import NanoEventsFactory, NanoAODSchema
+
+
+import numpy as np
+import os
+from optparse import OptionParser
+import concurrent.futures
+
+def DeltaR2(eta1, phi1, eta2, phi2):
+    deta2 = (eta1 - eta2)**2
+    dphi = phi1 - phi2
+    if dphi < -np.pi:
+        dphi += 2 * np.pi
+    elif dphi > np.pi:
+        dphi -= 2 * np.pi
+    return deta2 + dphi**2
+
+
+def future_savez(i,dataset):
+    #event = events_selected[i]
+    muons = events_selected.Muon[events_selected.Muon.istight]
+    electrons = events_selected.Electron[events_selected.Electron.istight]
+    leptons = []
+    leptons_px = 0.
+    leptons_py = 0.
+    for ilep in range(options.n_leptons_subtract):
+        if ilep < ak.size(muons[i]):
+            leptons_px += muons.pt[i][ilep] * np.cos(muons.phi[i][ilep])
+            leptons_py += muons.pt[i][ilep] * np.sin(muons.phi[i][ilep])
+            leptons.append(muons[i][ilep])
+        #else:
+        if ilep < ak.size(electrons[i]):
+            leptons_px += electrons.pt[i][ilep] * np.cos(electrons.phi[i][ilep])
+            leptons_py += electrons.pt[i][ilep] * np.sin(electrons.phi[i][ilep])
+            leptons.append(electrons[i][ilep])
+    #print(leptons_px, leptons_py, muons_selected[i].size, electrons_selected[i].size)
+
+    genmet_list = [
+        events_selected.GenMET.pt[i] * np.cos(events_selected.GenMET.phi[i]) + leptons_px,
+        events_selected.GenMET.pt[i] * np.sin(events_selected.GenMET.phi[i]) + leptons_py,
+        events_selected.MET.pt[i] * np.cos(events_selected.MET.phi[i]) + leptons_px,
+        events_selected.MET.pt[i] * np.sin(events_selected.MET.phi[i]) + leptons_py,
+        events_selected.PuppiMET.pt[i] * np.cos(events_selected.PuppiMET.phi[i]) + leptons_px,
+        events_selected.PuppiMET.pt[i] * np.sin(events_selected.PuppiMET.phi[i]) + leptons_py,
+        events_selected.DeepMETResponseTune.pt[i] * np.cos(events_selected.DeepMETResponseTune.phi[i]) + leptons_px,
+        events_selected.DeepMETResponseTune.pt[i] * np.sin(events_selected.DeepMETResponseTune.phi[i]) + leptons_py,
+        events_selected.DeepMETResolutionTune.pt[i] * np.cos(events_selected.DeepMETResolutionTune.phi[i]) + leptons_px,
+        events_selected.DeepMETResolutionTune.pt[i] * np.sin(events_selected.DeepMETResolutionTune.phi[i]) + leptons_py
+    ]
+
+    event_list = []
+    n_particles = len(events_selected.PFCands.pt[i])
+    nevents = len(events_selected)
+    print('Event:',i,'of ', nevents,'number of PF candidates:',n_particles)
+
+    '''
+    delta r matching
+    '''
+    for j in range(n_particles):
+        islepton = False
+        for jlep in range(options.n_leptons_subtract):
+            dr2 = DeltaR2(events_selected.PFCands.eta[i][j], events_selected.PFCands.phi[i][j], leptons[jlep].eta, leptons[jlep].phi)
+            if dr2 < 0.0001:
+                islepton = True
+                break
+        if not islepton:
+            particle_list = [
+                events_selected.PFCands.pt[i][j],
+                events_selected.PFCands.eta[i][j],
+                events_selected.PFCands.phi[i][j],
+                events_selected.PFCands.mass[i][j],
+                events_selected.PFCands.d0[i][j],
+                events_selected.PFCands.dz[i][j],
+                events_selected.PFCands.pdgId[i][j],
+                events_selected.PFCands.charge[i][j],
+                events_selected.PFCands.pvAssocQuality[i][j],
+                events_selected.PFCands.puppiWeight[i][j]
+            ]
+            event_list.append(particle_list)
+        #else:
+        #    print ("jlep: ", jlep, " dr2 ", dr2)
+
+    npz_file = '/hildafs/projects/phy230010p/xiea/npzs/'+dataset+'/raw/'+dataset+'_nevent_'+str(i)+'_nParticles_'+str(len(events_selected))
+    print('Saving file', npz_file+'.npz')
+    return np.savez(npz_file, x = np.array(event_list), y = np.array(genmet_list))
+
+'''
+select events with numbers of leptons >= nlepcut
+'''
+def SelectEvent(nlepcut):
+
+    
+    #select muons
+    
+    
+    select_tight_muon = ((events.Muon.tightId == 1) & (
+        events.Muon.pfRelIso03_all < 0.15) & (events.Muon.pt > 20.)) #mask for selecting tight muons
+
+    muonCopy = events.Muon[:]
+    muonCopy['istight'] = select_tight_muon #muons per event that passes the mask
+
+    events['Muon']=muonCopy[muonCopy.istight]
+    
+    # select electrons
+    select_tight_electron = (
+        (events.Electron.mvaFall17V1Iso_WP80 == 1) & (events.Electron.pt > 20.0))
+    
+    electronCopy = events.Electron[:]
+    electronCopy['istight'] = select_tight_electron #electrons per event that passes the mask
+
+    events['Electron'] = electronCopy[electronCopy.istight]
+
+    num_leptons = ak.count(events.Electron.pt,axis=-1) + ak.count(events.Muon.pt,axis=-1)
+        
+    events_selected = events[num_leptons >= nlepcut]
+
+    return events_selected
+
+
+if __name__ == '__main__':
+
+    parser = OptionParser()
+    parser.add_option('-d', '--dataset', help='dataset', dest='dataset', default='Test')
+    parser.add_option('--n_leptons', dest='n_leptons',
+                      help='How many leptons are required in the events', default=2)
+    parser.add_option('--n_leptons_subtract', dest='n_leptons_subtract',
+                      help='How many leptons to be subtracted from the Candidates list. Can not be larger than the n_leptons', default=2)
+    parser.add_option('--nn',help ='number of nano aod file, e.g 10', dest='nn',default=0 )
+    (options, args) = parser.parse_args()
+
+    dataset=options.dataset
+    nanoNumber=options.nn
+
+    datasetsname = {'dy':'/hildafs/projects/phy230010p/share/DYJetsToLL/','tt':'/hildafs/projects/phy230010p/share/TTTo2L2Nu/'}
+    if dataset not in datasetsname.keys():
+            print('choose one of them: ', datasetsname.keys())
+            exit()
+
+    assert options.n_leptons >= options.n_leptons_subtract, "n_leptons_subtract can not be larger than n_leptons"
+
+    fname = datasetsname[dataset]+'output_nano_'+str(nanoNumber)+'.root'
+    print('Opening file:', fname)
+
+    events = NanoEventsFactory.from_root(fname, schemaclass=NanoAODSchema).events()
+
+    events_selected = SelectEvent(options.n_leptons)
+   
+    n_events = ak.num(events_selected.Muon.pt,axis=0)
+
+    print('Total events selected:', n_events)
+
+    
+    for i in range(n_events):
+    #for i in range(10):
+        future_savez(i,dataset)
+    '''
+    with concurrent.futures.ProcessPoolExecutor(max_workers=8) as executor:
+        futures = set()
+        futures.update(executor.submit(future_savez, i) for i in range(n_events))
+        try:
+            total = len(futures)
+            processed = 0
+            while len(futures) > 0:
+                finished = set(job for job in futures if job.done())
+                for job in finished:
+                    job.result()
+                futures -= finished
+            del finished
+        except KeyboardInterrupt:
+            print("Ok quitter")
+            for job in futures: job.cancel()
+        except:
+            for job in futures: job.cancel()
+            raise
+    '''
